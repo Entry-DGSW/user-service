2112f8b00fb06af91e896d3b0c36be4f1fa22983
krung2 <jungbin4337@dgsw.hs.kr>
2022-03-23 11:04:30 +0900

    Feat: user consuming controller

---
+++ src/main/java/com/sugang/user/controller/user/UserController.java
@@ -0,0 +1,20 @@
+package com.sugang.user.controller.user;
+
+import com.sugang.user.common.kafka.UserSink;
+import com.sugang.user.controller.user.dto.request.AuthUserRequest;
+import com.sugang.user.service.user.AddUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final AddUserService addUserService;
+
+    @StreamListener(UserSink.INPUT)
+    public void putUser(AuthUserRequest authUserRequest) {
+        addUserService.addUser(authUserRequest);
+    }
+}
